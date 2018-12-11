780196dff2543fd2fd36a45a77cecafb86ad5264
yotam.bloom <yotam.bloom@il-059237-mbp.local>
2018-12-11 10:30:23 +0200

    deploy weeat project v5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.public_file_server.enabled = true
+  config.assets.compile = true
 end
